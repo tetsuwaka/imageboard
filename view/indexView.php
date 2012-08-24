e743abce82ce974623ad79c6783474e8f73e1d0b
tetsuwaka <tetsuwaka@gmail.com>
2012-08-24 14:29:30 +0900

    修正

--- view/indexView.php
+++ view/indexView.php
@@ -64,7 +64,7 @@
                 if (data.pagenum) {
                     var element = document.getElementById('livewrite');
                     element.innerHTML = '<img src="img/livewrite.png">';
-                    element.addEventListener('load', (document.getElementsByTagName('form')[0].submit()), false);
+                    element.addEventListener('click', (document.getElementsByTagName('form')[0].submit()), false);
                 }
             });
         </script>
