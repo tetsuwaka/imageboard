aa1ff272797f3b6c95456656f35389c8ba80a4e4
tetsuwaka <tetsuwaka@gmail.com>
2012-08-26 19:45:23 +0900

    修正

--- dbmanager/Image.php
+++ dbmanager/Image.php
@@ -5,8 +5,8 @@ require_once '../core/DbManager.php';
 class imageDB extends DbManager {
 
     public function getThread($num = 0) {
-        $sql = "select * from thread order by date desc limit 5 offset :num";
-        return $this->fetchAll($sql, array(':num' => $num));
+        $sql = "select * from thread order by date desc limit 5 offset {$num}";
+        return $this->fetchAll($sql);
     }
 
     public function getCommentsById($threadid) {
