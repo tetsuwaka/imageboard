4726500a79ebae3a2bc3874fd8a23eb7626461e4
tetsuwaka <tetsuwaka@gmail.com>
2012-08-31 11:15:54 +0900

    修正

--- view/drawView.php
+++ view/drawView.php
@@ -17,12 +17,9 @@
             <canvas id="myCanvas" width="480" height="320">
                 HTML5　Canvasに対応したブラウザーを使用してください。
             </canvas>
-            <div id="setarea">
-                <p>現在の設定</p>
-                <canvas id="setting" width="100" height="100">
-                    HTML5　Canvasに対応したブラウザーを使用してください。
-                </canvas>
-            </div>
+            <canvas id="setting" width="100" height="100">
+                HTML5　Canvasに対応したブラウザーを使用してください。
+            </canvas>
             <canvas id="save" width="240" height="160">
                 <script type="text/javascript">
                     var canvas = document.getElementById('save');
